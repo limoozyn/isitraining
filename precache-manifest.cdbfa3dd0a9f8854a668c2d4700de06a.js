4d2df0d2fa1b9f58050160955e3bdd88ae92a4fa
slava-zfort <v.miroshnychenko@zfort.com>
2019-11-12 13:48:49 +0200

    Updates

---
+++ precache-manifest.cdbfa3dd0a9f8854a668c2d4700de06a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a3414231479f965c19c93bffe064aba",
+    "url": "/isitraining/index.html"
+  },
+  {
+    "revision": "6d0711c94954f8dd91a0",
+    "url": "/isitraining/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "21948d0af4cb93c92e72",
+    "url": "/isitraining/static/js/2.2f32c074.chunk.js"
+  },
+  {
+    "revision": "6d0711c94954f8dd91a0",
+    "url": "/isitraining/static/js/main.51069a63.chunk.js"
+  },
+  {
+    "revision": "bbb5ad8e51fe8402c163",
+    "url": "/isitraining/static/js/runtime-main.370a87a0.js"
+  }
+]);
