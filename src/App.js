4123414fc09bb222841c84efd4bbc683a34ce8b2
slava-zfort <v.miroshnychenko@zfort.com>
2019-11-12 15:13:59 +0200

    fix: use HashRouter to make app work on github pages

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from "react";
 
 import "./App.css";
 import Navbar from "./components/elements/Navbar";
-import {BrowserRouter} from "react-router-dom";
+// import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import MainRoutes from "./routes";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter basename="/">
         <Navbar />
         <MainRoutes />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
